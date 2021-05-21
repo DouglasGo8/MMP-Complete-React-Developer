7e6d7980ab6c0e60ce9ded2a9acd0da505467056
DouglasGo8 <sgtear@hotmail.com>
2021-05-20 21:57:34 -0300

    Monters Rodolex start..

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+import "./App.css";
+
+/**
+ *
+ * @returns App first Main Component
+ */
+const app = () => {
+  return <Fragment>Hi from React.js App</Fragment>;
+};
+
+export default app;
